d6dbf76921e5e1df5ded0862df751b995fbbd51a
Harshad Hegde <hegdehb@gmail.com>
2022-11-01 13:40:34 -0500

    typo

--- docs/conf.py
+++ docs/conf.py
@@ -7,7 +7,7 @@ import os
 import re
 import sys
 from datetime import date
-from oakx-grape import __version__
+from oakx_grape import __version__
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
